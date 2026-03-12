9452c9f08867e365d73a04bf1e213e07dabc3861
dev1lfreak <kvachuk.sergey@yandex.ru>
2026-03-12 10:17:45 +0300

    fix : some changes in entities

--- entities/packet.py
+++ entities/packet.py
@@ -1,14 +1,7 @@
 class Packet:
-    """Объект сетевого пакета (фрагмент кадра)"""
-    def __init__(self, pts, media_type, payload_size, is_last):
-        self.pts = pts                      # Копируем PTS из кадра для сортировки в буфере
-        self.media_type = media_type        # 'video' или 'audio'
-        self.payload_size = payload_size    # Размер полезной нагрузки (не более MTU)
-        self.is_last = is_last              # Флаг: последний ли это пакет в кадре
-        
-        self.send_time = 0                  # Время отправки (t_send)
-        self.arrival_time = 0               # Время получения (t_recv)
-
-    def __repr__(self):
-        # Для удобного вывода в консоль при отладке
-        return f"[Packet {self.media_type}] PTS: {self.pts:.3f}s, Payload: {self.payload_size}"
+    def __init__(self, type, pts, size_bytes, send_time):
+        self.type = type
+        self.pts = pts
+        self.size_bytes = size_bytes
+        self.send_time = send_time # время отправки
+        self.arrival_time = 0 # время прибытия
